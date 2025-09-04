2e3eb85073fb81d906d592a1b2103cef0e3a1335
Lavanya B <54321029+lavanya027@users.noreply.github.com>
2025-09-05 00:37:31 +0530

    Update LoginPage.java

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,39 +1,39 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.How;
-import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
 
-/**
- * Created by Karthik on 21/09/2019.
- */
 public class LoginPage {
 
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    private By txtUserName = By.name("UserName");
+    private By txtPassword = By.name("Password");
+    private By btnLogin = By.name("Login");
+
     public LoginPage(WebDriver driver) {
-        PageFactory.initElements(driver, this);
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-    @FindBy(how = How.NAME, using = "UserName")
-    public WebElement txtUserName;
+    public void Login(String userName, String password) {
+        WebElement usernameField = wait.until(ExpectedConditions.visibilityOfElementLocated(txtUserName));
+        WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(txtPassword));
 
-    @FindBy(how = How.NAME, using = "Password")
-    public WebElement txtPassword;
+        usernameField.clear();
+        usernameField.sendKeys(userName);
 
-    @FindBy(how = How.NAME, using = "Login")
-    public WebElement btnLogin;
-
-    public void Login(String userName, String password)
-    {
-        txtUserName.sendKeys(userName);
-        txtPassword.sendKeys(password);
+        passwordField.clear();
+        passwordField.sendKeys(password);
     }
 
-    public void ClickLogin()
-    {
-        btnLogin.submit();
+    public void ClickLogin() {
+        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(btnLogin));
+        loginButton.click();
     }
-
-
 }
